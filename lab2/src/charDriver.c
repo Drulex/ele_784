ae73570de0c0eb3a51cbd4360966e2850f4490b9
Alexandru Jora <alexandru@jora.ca>
2015-10-12 11:58:31 -0400

    Increase verbosity and return actual number of bytes written.

--- lab2/src/charDriver.c
+++ lab2/src/charDriver.c
@@ -304,9 +304,12 @@ static ssize_t charDriver_write(struct file *flip, const char __user *ubuf, size
         // push data in circular buffer
         while(i<READWRITE_BUFSIZE && buf_retcode != -1){
             circularBufferIn(Buffer, charStruct->WriteBuf[i]);
+            printk(KERN_WARNING "===charDriver_write: circularBufferIn=%i\n", buf_retcode);
             i++;
         }
-        return READWRITE_BUFSIZE;
+        printk(KERN_WARNING "===charDriver_write: contents of WriteBuf:%s\n", charStruct->WriteBuf);
+        // we return only the number of bytes pushed to circular buffer
+        return i;
     }
 
 
