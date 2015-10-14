dc5cb8a30e42ed3a99a82df4a07f5ae299bdb726
Alexandru Jora <alexandru@jora.ca>
2015-10-14 17:44:44 -0400

    Work in progress

--- lab2/src/testchardriver.c
+++ lab2/src/testchardriver.c
@@ -16,65 +16,54 @@
 #include <string.h>
 
 #define DEVICE_NODE     "/dev/charDriver_Node"
-#define IN_BUF_SIZE     10
-#define OUT_BUF_SIZE    10
 
 // lazy terminal clearing
-#define CLEAR_TERM  printf("\033[2J\033[1;1H");
+#define CLEAR_TERM  printf("\033[2J\033[1;1H")
+static int fd;
 
 int main(void) {
     int opened = 0;
     while(1){
         int choice = menu();
         switch(choice){
-            case 1:
-                open_mode();
-                break;
 
-            case 2:
-                if(!opened){
-                    printf("***Error you must open device first!***\n\n");
-                    break;
-                }
+            case 1:
                 read_mode();
                 break;
 
-            case 3:
-                if(!opened){
-                    printf("***Error you must open device first!***\n\n");
-                    break;
-                }
+            case 2:
                 write_mode();
                 break;
 
-            case 4:
-                if(!opened){
-                    printf("***Error you must open device first!***\n\n");
-                    break;
-                }
+            case 3:
                 printf("Not implemented!\n");
                 break;
 
-            case 5:
+            case 4:
                 printf("Exiting\n");
                 exit(-1);
                 break;
+
+            default:
+                printf("Command not recognized! Exiting\n");
+                exit(-1);
         }
+        close(fd);
     }
 }
 
 int menu(void){
+    CLEAR_TERM;
     int cmd;
     cmd = 0;
     printf("===========================\n");
     printf("charDriver test application\n");
     printf("===========================\n\n");
     printf("Choose a command to perform\n");
-    printf("1. Open\n");
-    printf("2. Read\n");
-    printf("3. Write\n");
-    printf("4. Send IOCTL command\n");
-    printf("5. Exit\n");
+    printf("1. Read\n");
+    printf("2. Write\n");
+    printf("3. Send IOCTL command\n");
+    printf("4. Exit\n");
     scanf("%d", &cmd);
     CLEAR_TERM;
     return cmd;
@@ -90,17 +79,47 @@ int read_blocking(void){
 }
 
 int read_non_blocking(void){
-    int numBytes;
+    int numBytes, i;
+    int redo = 0;
     printf("Reading in non blocking mode\n");
     printf("How many bytes to read?\n");
     scanf("%d", &numBytes);
+    char bufOut[numBytes];
+    // flush buffer
+    for(i=0; i<numBytes; i++){
+        bufOut[i] = '\0';
+    }
     //TODO
-    return 0;
+    if(fd){
+        read(fd, &bufOut, numBytes+1);
+        printf("Read from buffer: %s\n\n", bufOut);
+    }
+    else{
+        printf("Error using file descriptor!\n");
+        exit(-2);
+    }
+    printf("Read again?\n");
+    printf("1. Yes\n");
+    printf("2. No\n");
+    scanf("%d", &redo);
+    if(redo == 1)
+        read_non_blocking();
+    else
+        return 0;
 }
 
 int read_mode(void){
     int read_mode;
     read_mode = 0;
+    fd = open(DEVICE_NODE, O_RDONLY);
+    if(fd){
+        printf("Device succesfully openend in O_RDONLY mode\n\n");
+    }
+    else{
+        printf("Error opening device in O_RDONLY mode\n");
+        printf("Exiting...\n");
+        exit(-1);
+    }
     printf("Choose an opening mode\n");
     printf("1. Blocking\n");
     printf("2. Non-blocking\n");
@@ -129,17 +148,49 @@ int write_blocking(void){
 }
 
 int write_non_blocking(void){
-    int numBytes;
+    int numBytes, i;
+    int redo = 0;
     printf("Writing in non blocking mode\n");
     printf("How many bytes to write?\n");
     scanf("%d", &numBytes);
-    //TODO
-    return 0;
+    char bufIn[numBytes + 1];
+    // flush buffer
+    for(i=0; i<numBytes; i++){
+        bufIn[i] = '\0';
+    }
+    printf("Data to be written?\n");
+    scanf("%s", bufIn);
+
+    if(fd){
+        write(fd, &bufIn, numBytes);
+        printf("Write to buffer: %s\n\n", bufIn);
+    }
+    else{
+        printf("Error using file descriptor!\n");
+        exit(-2);
+    }
+    printf("Write again?\n");
+    printf("1. Yes\n");
+    printf("2. No\n");
+    scanf("%d", &redo);
+    if(redo == 1)
+        write_non_blocking();
+    else
+        return 0;
 }
 
 int write_mode(void){
     int write_mode;
     write_mode = 0;
+    fd = open(DEVICE_NODE, O_WRONLY);
+    if(fd){
+        printf("Device succesfully openend in O_RWONLY mode\n\n");
+    }
+    else{
+        printf("Error opening device in O_RWONLY mode\n");
+        printf("Exiting...\n");
+        exit(-1);
+    }
     printf("Choose an opening mode\n");
     printf("1. Blocking\n");
     printf("2. Non-blocking\n");
@@ -158,90 +209,6 @@ int write_mode(void){
     return 0;
 }
 
-int open_mode(void){
-    int open_mode;
-    int fd;
-    open_mode = 0;
-    printf("Choose an opening mode\n");
-    printf("1. O_RDONLY\n");
-    printf("2. O_WRONLY\n");
-    printf("3. O_RDRW\n");
-    scanf("%d", &open_mode);
-    CLEAR_TERM;
-
-    switch(open_mode){
-        case 1:
-            // open read only
-            fd = open(DEVICE_NODE, O_RDONLY);
-            if(fd){
-                printf("Device succesfully openend in O_RDONLY mode\n");
-            }
-            else{
-                printf("Error opening device in O_RDONLY mode\n");
-                printf("Exiting...\n");
-                exit(-1);
-            }
-            read_mode();
-            break;
-
-        case 2:
-            // open write only
-            fd = open(DEVICE_NODE, O_WRONLY);
-            if(fd){
-                printf("Device succesfully openend in O_RWONLY mode\n");
-            }
-            else{
-                printf("Error opening device in O_RWONLY mode\n");
-                printf("Exiting...\n");
-                exit(-1);
-            }
-            write_mode();
-            break;
-
-        case 3:
-            // open read/write
-            fd = open(DEVICE_NODE, O_RDWR);
-            if(fd){
-                printf("Device succesfully openend in O_RDRW mode\n");
-            }
-            else{
-                printf("Error opening device in O_RDRW mode\n");
-                printf("Exiting...\n");
-                exit(-1);
-            }
-            rw_mode();
-            break;
-    }
-    return 0;
-}
-
-int rw_mode(void){
-    int rw_mode;
-    rw_mode = 0;
-    printf("Choose a command\n");
-    printf("1. Read\n");
-    printf("2. Write\n");
-    printf("3. Exit\n");
-    scanf("%d", &rw_mode);
-    CLEAR_TERM;
-
-    do{
-        switch(rw_mode){
-            case 1:
-                read_mode();
-                break;
-
-            case 2:
-                write_mode();
-                break;
-
-            case 3:
-                printf("Exiting R/W mode\n");
-                return 0;
-        }
-    } while(!rw_mode);
-    return 0;
-}
 
 
     /*
